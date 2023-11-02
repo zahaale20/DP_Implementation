5ac1b4b3196ebe038dd3aae66f757657c4547316
zahaale20 <alexcz@outlook.com>
2023-11-02 09:07:44 -0700

    import, foundation

--- open_dp_implementation.py
+++ open_dp_implementation.py
@@ -1,1 +1,30 @@
 import opendp
+import pandas as pd
+
+df = pd.read_csv("forbes_2640_billionaires.csv")
+df.columns = ['rank', 
+              'name', 
+              'forbes_id', 
+              'net_worth', 
+              'age', 
+              'age_range', 
+              'country', 
+              'source', 
+              'industry', 
+              'Age', 
+              'Source of Wealth', 
+              'Self-Made Score', 
+              'Philanthropy Score', 
+              'Residence', 
+              'Citizenship', 
+              'Marital Status', 
+              'Children', 
+              'Education', 
+              'Bachelor', 
+              'Master', 
+              'Doctorate', 
+              'Drop Out', 
+              'Self Made']
+
+del df["name"]
+del df["forbes_id"]
